bfbc7a766bae155a875a07e5735dfbe45b5fe38d
Vishwas Paliwal <vishwaspaliwal2005@gmail.com>
2026-03-30 16:39:36 +0530

    Update mul_test.v

--- shift_add_mul/mul_test.v
+++ shift_add_mul/mul_test.v
@@ -21,14 +21,13 @@
 
 
 module mul_test();
-reg [15:0] a, q;
+    reg [15:0] a, q;
     reg clk, start;
 
     // Outputs
     wire done;
     wire [32:0] P;
 
-    // Instantiate your multiplier
     multiplier uut (
         .P(P), 
         .done(done), 
@@ -38,28 +37,24 @@ reg [15:0] a, q;
         .clk(clk)
     );
 
-    // Generate Clock: 10ns period (100MHz)
     initial clk = 0;
     always #5 clk = ~clk;
 
     initial begin
         // 1. Initialize
-        a = 16'd11;     // Multiplicand
-        q = 16'd13;     // Multiplier
+        a = 16'd11;
+        q = 16'd13;
         start = 0;
         
-        #20;            // Wait a bit
+        #20;
         
-        // 2. Pulse Start for one clock cycle
         @(posedge clk);
         start = 1;
         @(posedge clk);
         start = 0;
 
-        // 3. Wait for the 'done' signal
         wait(done);
         
-        // 4. Display result
         #5; 
         $display("Calculation Finished!");
         $display("Input A: %d, Input Q: %d", a, q);
@@ -71,7 +66,7 @@ reg [15:0] a, q;
             $display("FAILURE: Expected 143, got %d", P);
 
         #20;
-        $finish;        // Stop the simulation
+        $finish;
     end
       
 endmodule
